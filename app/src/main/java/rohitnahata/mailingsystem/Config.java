b4930dd79db4f6eb51b62927e64885aa4b93d543
Rohit Nahata <rnahata1997@gmail.com>
2016-09-12 14:53:30 +0530

    removed config

--- app/src/main/java/rohitnahata/mailingsystem/Config.java
+++
@@ -1,9 +0,0 @@
-package rohitnahata.mailingsystem;
-
-/**
- * Created by Rohit on 03/09/2016.
- */
-public class Config {
-    public static final String EMAIL ="batchc2018@gmail.com";
-    public static final String PASSWORD ="my_college_mailing_system";
-}
